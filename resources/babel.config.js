97446c8c10dfb1c786e3d1344b35c79acda2c310
zdm <zdm@softvisio.net>
2020-11-25 01:34:48 +0200

    babel config updated

--- resources/babel.config.js
+++ resources/babel.config.js
@@ -2,6 +2,14 @@ module.exports = function ( api ) {
     api.cache( true );
 
     return {
-        "presets": ["@vue/app"],
+        "presets": [
+            [
+                "@babel/preset-env",
+                {
+                    "shippedProposals": true,
+                },
+            ],
+            "@vue/app",
+        ],
     };
 };
