b6a5a6ea0d2cae343a2f93dfd8fa87b5bf6915b7
zdm <zdm@softvisio.net>
2020-11-29 17:12:21 +0200

    babel config updated to use decorators stage 2

--- resources/babel.config.js
+++ resources/babel.config.js
@@ -3,13 +3,8 @@ module.exports = function ( api ) {
 
     return {
         "presets": [
-            [
-                "@babel/preset-env",
-                {
-                    "shippedProposals": true,
-                },
-            ],
-            "@vue/app",
+            ["@babel/preset-env", { "shippedProposals": true }],
+            ["@vue/app", { "decoratorsLegacy": false, "decoratorsBeforeExport": true }],
         ],
     };
 };
