6df5fc55525680baa6be41bacd91df76ee4a9170
zdm <zdm@softvisio.net>
2021-09-09 17:54:25 +0300

    fix: vue config

--- resources/vue.config.cjs
+++ resources/vue.config.cjs
@@ -1,7 +1,3 @@
-// https://cli.vuejs.org/config/
-
-// const webpack = require( "webpack" );
-
 process.env.VUE_APP_BUILD_CORDOVA = process.env.VUE_APP_BUILD_CORDOVA === "true" ? "true" : "";
 process.env.DEVSERVER_POLL = process.env.DEVSERVER_POLL === "true" ? "true" : "";
 
@@ -52,25 +48,9 @@ const vueConfig = {
 
         // aliases
         config.resolve.alias[":softvisio"] = "@softvisio/vue";
-
-        // global vars
-        // config.plugins.push( new webpack.ProvidePlugin( {
-        //     "Ext": config.resolve.alias["#ext.js"],
-        // } ) );
     },
 
     "chainWebpack": config => {
-
-        // add webpack-bundle-analyzer
-        const bundleAnalyzerPlugin = require( "vue-cli-plugin-webpack-bundle-analyzer" );
-        bundleAnalyzerPlugin( { "chainWebpack": callback => callback( config ) }, vueConfig );
-
-        // clear exclude for "babel-loader"
-        // config.module
-        //     .rule( "js" )
-        //     .exclude.clear()
-        //     .end();
-
         if ( process.env.NODE_ENV === "development" || process.env.VUE_APP_BUILD_CORDOVA ) {
 
             // exclude ext from babel-loader
@@ -103,16 +83,6 @@ const vueConfig = {
             .test( /\.worker\.js$/ )
             .use( "worker-loader" )
             .loader( "worker-loader" );
-
-        if ( process.env.NODE_ENV === "production" ) {
-
-            // XXX not required for webpack5
-            // configure html minification, https://github.com/kangax/html-minifier#options-quick-reference
-            // config.plugin( "html" ).tap( args => {
-            //     args[0].minify.removeAttributeQuotes = false;
-            //     return args;
-            // } );
-        }
 
         // disable WebpackBundleAnalyzer
         if ( process.env.NODE_ENV === "production" || process.env.VUE_APP_BUILD_CORDOVA ) config.plugins.delete( "webpack-bundle-analyzer" );
