13155d41ba8a6b3f56994f1f159a28c93e1f69f8
zdm <zdm@softvisio.net>
2021-09-17 07:12:48 +0300

    refactor: viewport init

--- lib/viewport.vue
+++ lib/viewport.vue
@@ -11,21 +11,26 @@ export default {
     },
 
     async mounted () {
-
-        // init session
-        while ( true ) {
-            const res = await this.$store.session.signin();
-
-            // connection ok
-            if ( res.ok || res.status === 401 || res.status === 403 ) break;
-
-            // connection error
-            await this.onAppInitFailure();
-        }
+        this.init();
     },
 
     "methods": {
-        async onAppInitFailure () {
+        async init () {
+            console.log( "--- MOUN 1" );
+
+            // init session
+            while ( true ) {
+                const res = await this.$store.session.signin();
+
+                // connection ok
+                if ( res.ok || res.status === 401 || res.status === 403 ) break;
+
+                // connection error
+                await this.onInitFailure();
+            }
+        },
+
+        async onInitFailure () {
             alert( `Server connection error. Press "OK" to try again.` );
         },
     },
