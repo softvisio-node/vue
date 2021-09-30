8418bfeda9b4357e389d36bc97218e6e9dbe2bc3
zdm <zdm@softvisio.net>
2021-09-30 13:38:44 +0300

    feat: webpack migration

--- resources/webpack.config.vue-app.js
+++ resources/webpack.config.vue-app.js
@@ -64,13 +64,13 @@ const config = {
         "splitChunks": {
             "cacheGroups": {
                 "defaultVendors": {
-                    "name": "chunk-vendors",
+                    "name": "vendors",
                     "test": /[\\/]node_modules[\\/]/,
                     "priority": -10,
                     "chunks": "initial",
                 },
                 "common": {
-                    "name": "chunk-common",
+                    "name": "common",
                     "minChunks": 2,
                     "priority": -20,
                     "chunks": "initial",
