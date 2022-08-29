f3593f9a9d8006c9604d456ecfbe9dff2a4b3d22
zdm <zdm@softvisio.net>
2022-08-29 23:21:22 +0300

    feat: webpack config refactored

--- resources/webpack/firebase.worker.config.js
+++ resources/webpack/firebase.worker.config.js
@@ -2,6 +2,7 @@
 //     "scope": "/firebase-cloud-messaging-push-scope",
 // } );
 
+import WebpackConfig from "#lib/webpack/config";
 import env from "#core/env";
 import webpack from "webpack";
 import TerserPlugin from "terser-webpack-plugin";
@@ -11,77 +12,78 @@ const DefinePlugin = webpack.DefinePlugin;
 
 const worker = resolve( "#lib/firebase/firebase.worker", import.meta.url );
 
-const config = {
-    "schemas": [
+export default new WebpackConfig(
+    options => {
+        return {
+            "name": "firebase.worker",
+            "target": "webworker",
+            "mode": global.WEBPACK.MODE,
+            "context": global.WEBPACK.CONTEXT,
+            "devtool": env.isDevelopment ? "eval-source-map" : undefined,
+            "experiments": { "topLevelAwait": true },
+            "cache": global.WEBPACK.CACHE_OPTIONS,
+
+            "entry": {
+                "firebase": {
+                    "import": worker,
+                    "filename": "firebase-messaging.worker.js",
+                },
+            },
+
+            "output": {
+                "path": global.WEBPACK.TMP_PATH,
+                "publicPath": "auto",
+            },
+
+            "resolve": {
+                "alias": {
+                    ...global.WEBPACK.RESOLVE_ALIAS,
+                    "#vue": "@softvisio/vue",
+                },
+
+                // required by froala, can be replaced with crypto-browserify
+                "fallback": {
+                    "crypto": false,
+                },
+
+                "extensions": [".mjs", ".js", ".jsx", ".vue", ".json", ".wasm"],
+
+                "modules": global.WEBPACK.RESOLVE_MODULES,
+            },
+
+            "resolveLoader": { "modules": global.WEBPACK.RESOLVE_LOADER_MODULES },
+
+            "optimization": {
+                "minimizer": [new TerserPlugin( global.WEBPACK.TERSER_OPTIONS )],
+            },
+
+            "module": {
+                "rules": [
+
+                    // js
+                    {
+                        "test": /\.[cm]?jsx?$/,
+                        "resolve": {
+                            "fullySpecified": false,
+                        },
+                        "loader": "babel-loader",
+                        "options": global.WEBPACK.BABEL_OPTIONS,
+                    },
+                ],
+            },
+
+            "plugins": [
+                new DefinePlugin( {
+                    "process.env": global.WEBPACK.ENV,
+                    "process.config": global.WEBPACK.CONFIG,
+                } ),
+            ],
+        };
+    },
+    [
 
         //
         new URL( "../schemas/env.main.schema.yaml", import.meta.url ),
         new URL( "../schemas/env.firebase.worker.schema.yaml", import.meta.url ),
-    ],
-
-    "name": "firebase.worker",
-    "target": "webworker",
-    "mode": global.WEBPACK.MODE,
-    "context": global.WEBPACK.CONTEXT,
-    "devtool": env.isDevelopment ? "eval-source-map" : undefined,
-    "experiments": { "topLevelAwait": true },
-    "cache": global.WEBPACK.CACHE_OPTIONS,
-
-    "entry": {
-        "firebase": {
-            "import": worker,
-            "filename": "firebase-messaging.worker.js",
-        },
-    },
-
-    "output": {
-        "path": global.WEBPACK.TMP_PATH,
-        "publicPath": "auto",
-    },
-
-    "resolve": {
-        "alias": {
-            ...global.WEBPACK.RESOLVE_ALIAS,
-            "#vue": "@softvisio/vue",
-        },
-
-        // required by froala, can be replaced with crypto-browserify
-        "fallback": {
-            "crypto": false,
-        },
-
-        "extensions": [".mjs", ".js", ".jsx", ".vue", ".json", ".wasm"],
-
-        "modules": global.WEBPACK.RESOLVE_MODULES,
-    },
-
-    "resolveLoader": { "modules": global.WEBPACK.RESOLVE_LOADER_MODULES },
-
-    "optimization": {
-        "minimizer": [new TerserPlugin( global.WEBPACK.TERSER_OPTIONS )],
-    },
-
-    "module": {
-        "rules": [
-
-            // js
-            {
-                "test": /\.[cm]?jsx?$/,
-                "resolve": {
-                    "fullySpecified": false,
-                },
-                "loader": "babel-loader",
-                "options": global.WEBPACK.BABEL_OPTIONS,
-            },
-        ],
-    },
-
-    "plugins": [
-        new DefinePlugin( {
-            "process.env": global.WEBPACK.ENV,
-            "process.config": global.WEBPACK.CONFIG,
-        } ),
-    ],
-};
-
-export default config;
+    ]
+);
--- resources/webpack/main.config.js
+++ resources/webpack/main.config.js
@@ -1,3 +1,4 @@
+import WebpackConfig from "#lib/webpack/config";
 import env from "#core/env";
 import webpack from "webpack";
 import { VueLoaderPlugin } from "vue-loader";
@@ -11,251 +12,252 @@ import { readConfig } from "#core/config";
 
 const DefinePlugin = webpack.DefinePlugin;
 
-const config = {
-    "schemas": [new URL( "../schemas/env.main.schema.yaml", import.meta.url )],
+export default new WebpackConfig(
+    options => {
+        return {
+            "name": "main",
 
-    "name": "main",
+            // "target": "web", "browserslist",
+            "mode": global.WEBPACK.MODE,
+            "context": global.WEBPACK.CONTEXT,
+            "devtool": env.isDevelopment ? "eval-source-map" : undefined,
+            "experiments": { "topLevelAwait": true },
+            "cache": global.WEBPACK.CACHE_OPTIONS,
 
-    // "target": "web", "browserslist",
-    "mode": global.WEBPACK.MODE,
-    "context": global.WEBPACK.CONTEXT,
-    "devtool": env.isDevelopment ? "eval-source-map" : undefined,
-    "experiments": { "topLevelAwait": true },
-    "cache": global.WEBPACK.CACHE_OPTIONS,
-
-    "entry": {
-        "app": "./src",
-    },
-
-    "output": {
-        "path": global.WEBPACK.OUTPUT_PATH,
-        "publicPath": "auto",
-        "filename": "js/[name].[contenthash].js",
-        "chunkFilename": "js/[name].[contenthash].js",
-        "hashDigestLength": 8,
-    },
-
-    "resolve": {
-        "alias": {
-            ...global.WEBPACK.RESOLVE_ALIAS,
-            "vue$": "vue/dist/vue.runtime.esm-bundler.js",
-            "#vue": "@softvisio/vue",
-        },
-
-        // required by froala, can be replaced with crypto-browserify
-        "fallback": {
-            "crypto": false,
-        },
-
-        "extensions": [".mjs", ".js", ".jsx", ".vue", ".json", ".wasm", ".po", ".yaml"],
-
-        "modules": global.WEBPACK.RESOLVE_MODULES,
-    },
-
-    "resolveLoader": { "modules": global.WEBPACK.RESOLVE_LOADER_MODULES },
-
-    "optimization": {
-        "splitChunks": {
-            "cacheGroups": {
-                "vendors": {
-                    "name": "vendors",
-                    "test": /[\\/]node_modules[\\/]/,
-                    "priority": -10,
-                    "chunks": "initial",
-                },
-                "common": {
-                    "name": "common",
-                    "minChunks": 2,
-                    "priority": -20,
-                    "chunks": "initial",
-                    "reuseExistingChunk": true,
-                },
+            "entry": {
+                "app": "./src",
             },
-        },
 
-        "minimizer": [
-            new TerserPlugin( global.WEBPACK.TERSER_OPTIONS ),
+            "output": {
+                "path": global.WEBPACK.OUTPUT_PATH,
+                "publicPath": "auto",
+                "filename": "js/[name].[contenthash].js",
+                "chunkFilename": "js/[name].[contenthash].js",
+                "hashDigestLength": 8,
+            },
 
-            new CssMinimizerPlugin( {
-                "parallel": true,
-                "minimizerOptions": {
-                    "preset": [
-                        "default",
-                        {
-                            "discardComments": {
-                                "removeAll": true,
-                            },
+            "resolve": {
+                "alias": {
+                    ...global.WEBPACK.RESOLVE_ALIAS,
+                    "vue$": "vue/dist/vue.runtime.esm-bundler.js",
+                    "#vue": "@softvisio/vue",
+                },
+
+                // required by froala, can be replaced with crypto-browserify
+                "fallback": {
+                    "crypto": false,
+                },
+
+                "extensions": [".mjs", ".js", ".jsx", ".vue", ".json", ".wasm", ".po", ".yaml"],
+
+                "modules": global.WEBPACK.RESOLVE_MODULES,
+            },
+
+            "resolveLoader": { "modules": global.WEBPACK.RESOLVE_LOADER_MODULES },
+
+            "optimization": {
+                "splitChunks": {
+                    "cacheGroups": {
+                        "vendors": {
+                            "name": "vendors",
+                            "test": /[\\/]node_modules[\\/]/,
+                            "priority": -10,
+                            "chunks": "initial",
                         },
-                    ],
-                },
-            } ),
-        ],
-    },
-
-    "module": {
-        "rules": [
-
-            // js
-            {
-                "test": /\.[cm]?jsx?$/,
-                "resolve": {
-                    "fullySpecified": false,
-                },
-                "oneOf": [
-
-                    // web workers *.worker.js
-                    {
-                        "test": /\.worker\.js$/,
-                        "type": "asset/resource",
-                        "generator": {
-                            "filename": "[name].[hash][ext][query]",
+                        "common": {
+                            "name": "common",
+                            "minChunks": 2,
+                            "priority": -20,
+                            "chunks": "initial",
+                            "reuseExistingChunk": true,
                         },
                     },
+                },
 
-                    // other *.js files
-                    {
-                        "loader": "babel-loader",
-                        "options": global.WEBPACK.BABEL_OPTIONS,
-                    },
+                "minimizer": [
+                    new TerserPlugin( global.WEBPACK.TERSER_OPTIONS ),
+
+                    new CssMinimizerPlugin( {
+                        "parallel": true,
+                        "minimizerOptions": {
+                            "preset": [
+                                "default",
+                                {
+                                    "discardComments": {
+                                        "removeAll": true,
+                                    },
+                                },
+                            ],
+                        },
+                    } ),
                 ],
             },
 
-            // vue
-            {
-                "test": /\.vue$/,
-                "loader": "vue-loader",
-                "options": {
+            "module": {
+                "rules": [
 
-                    // XXX "babelParserPlugins": ["jsx", "classProperties", "decorators-legacy"],
-                    "compilerOptions": {
-                        "isCustomElement": tag => tag.startsWith( "ext-" ),
-                    },
-                },
-            },
+                    // js
+                    {
+                        "test": /\.[cm]?jsx?$/,
+                        "resolve": {
+                            "fullySpecified": false,
+                        },
+                        "oneOf": [
 
-            // images
-            {
-                "test": /\.(png|jpe?g|gif|webp|avif|svg)(\?.*)?$/,
-                "type": "asset/resource",
-                "generator": {
-                    "filename": "img/[name].[hash][ext][query]",
-                },
-            },
-
-            // media
-            {
-                "test": /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-                "type": "asset/resource",
-                "generator": {
-                    "filename": "media/[name].[hash][ext][query]",
-                },
-            },
-
-            // fonts
-            {
-                "test": /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
-                "type": "asset/resource",
-                "generator": {
-                    "filename": "fonts/[name].[hash][ext][query]",
-                },
-            },
-
-            // css
-            {
-                "test": /\.css$/,
-                "use": [
-                    process.env.WEBPACK_DEV_SERVER
-                        ? {
-                            "loader": "vue-style-loader",
-                            "options": {
-                                "sourceMap": false,
-                                "shadowMode": false,
+                            // web workers *.worker.js
+                            {
+                                "test": /\.worker\.js$/,
+                                "type": "asset/resource",
+                                "generator": {
+                                    "filename": "[name].[hash][ext][query]",
+                                },
                             },
-                        }
-                        : {
-                            "loader": MiniCssExtractPlugin.loader,
-                            "options": {
-                                "esModule": false,
+
+                            // other *.js files
+                            {
+                                "loader": "babel-loader",
+                                "options": global.WEBPACK.BABEL_OPTIONS,
+                            },
+                        ],
+                    },
+
+                    // vue
+                    {
+                        "test": /\.vue$/,
+                        "loader": "vue-loader",
+                        "options": {
+
+                            // XXX "babelParserPlugins": ["jsx", "classProperties", "decorators-legacy"],
+                            "compilerOptions": {
+                                "isCustomElement": tag => tag.startsWith( "ext-" ),
                             },
                         },
+                    },
+
+                    // images
                     {
-                        "loader": "css-loader",
-                        "options": {
-                            "sourceMap": false,
-                            "importLoaders": 2,
+                        "test": /\.(png|jpe?g|gif|webp|avif|svg)(\?.*)?$/,
+                        "type": "asset/resource",
+                        "generator": {
+                            "filename": "img/[name].[hash][ext][query]",
                         },
                     },
+
+                    // media
                     {
-                        "loader": "postcss-loader",
-                        "options": {
-                            "sourceMap": false,
-                            "postcssOptions": {
-                                "plugins": {
-                                    "cssnano": {
-                                        "preset": ["default", { "normalizeWhitespace": false }],
+                        "test": /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                        "type": "asset/resource",
+                        "generator": {
+                            "filename": "media/[name].[hash][ext][query]",
+                        },
+                    },
+
+                    // fonts
+                    {
+                        "test": /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+                        "type": "asset/resource",
+                        "generator": {
+                            "filename": "fonts/[name].[hash][ext][query]",
+                        },
+                    },
+
+                    // css
+                    {
+                        "test": /\.css$/,
+                        "use": [
+                            process.env.WEBPACK_DEV_SERVER
+                                ? {
+                                    "loader": "vue-style-loader",
+                                    "options": {
+                                        "sourceMap": false,
+                                        "shadowMode": false,
+                                    },
+                                }
+                                : {
+                                    "loader": MiniCssExtractPlugin.loader,
+                                    "options": {
+                                        "esModule": false,
+                                    },
+                                },
+                            {
+                                "loader": "css-loader",
+                                "options": {
+                                    "sourceMap": false,
+                                    "importLoaders": 2,
+                                },
+                            },
+                            {
+                                "loader": "postcss-loader",
+                                "options": {
+                                    "sourceMap": false,
+                                    "postcssOptions": {
+                                        "plugins": {
+                                            "cssnano": {
+                                                "preset": ["default", { "normalizeWhitespace": false }],
+                                            },
+                                        },
                                     },
                                 },
                             },
-                        },
+                        ],
+                    },
+
+                    // .po
+                    {
+                        "test": /\.po$/,
+                        "loader": "@softvisio/vue/po-loader",
+                        "options": { PoFile },
+                    },
+
+                    // .json
+                    {
+                        "test": /\.json$/,
+                        "loader": "@softvisio/vue/config-loader",
+                        "options": { readConfig, "type": "json" },
+                    },
+
+                    // .yaml
+                    {
+                        "test": /\.yaml$/,
+                        "loader": "@softvisio/vue/config-loader",
+                        "options": { readConfig, "type": "yaml" },
                     },
                 ],
             },
 
-            // .po
-            {
-                "test": /\.po$/,
-                "loader": "@softvisio/vue/po-loader",
-                "options": { PoFile },
-            },
+            "plugins": [
+                new VueLoaderPlugin(),
 
-            // .json
-            {
-                "test": /\.json$/,
-                "loader": "@softvisio/vue/config-loader",
-                "options": { readConfig, "type": "json" },
-            },
+                new MiniCssExtractPlugin( {
+                    "filename": "css/[name].[contenthash].css",
+                    "chunkFilename": "css/[name].[contenthash].css",
+                } ),
 
-            // .yaml
-            {
-                "test": /\.yaml$/,
-                "loader": "@softvisio/vue/config-loader",
-                "options": { readConfig, "type": "yaml" },
-            },
-        ],
-    },
+                new DefinePlugin( {
+                    "__VUE_OPTIONS_API__": "true",
+                    "__VUE_PROD_DEVTOOLS__": "false",
+                    "process.env": global.WEBPACK.ENV,
+                    "process.config": global.WEBPACK.CONFIG,
+                } ),
 
-    "plugins": [
-        new VueLoaderPlugin(),
+                new HtmlPlugin( {
+                    "scriptLoading": "defer",
+                    "template": "public/index.html",
+                    "templateParameters": process.env,
+                } ),
 
-        new MiniCssExtractPlugin( {
-            "filename": "css/[name].[contenthash].css",
-            "chunkFilename": "css/[name].[contenthash].css",
-        } ),
-
-        new DefinePlugin( {
-            "__VUE_OPTIONS_API__": "true",
-            "__VUE_PROD_DEVTOOLS__": "false",
-            "process.env": global.WEBPACK.ENV,
-            "process.config": global.WEBPACK.CONFIG,
-        } ),
-
-        new HtmlPlugin( {
-            "scriptLoading": "defer",
-            "template": "public/index.html",
-            "templateParameters": process.env,
-        } ),
-
-        new CopyPlugin( {
-            "patterns": [
-                {
-                    "from": "public",
-                    "globOptions": {
-                        "ignore": ["**/index.html"],
-                    },
-                },
+                new CopyPlugin( {
+                    "patterns": [
+                        {
+                            "from": "public",
+                            "globOptions": {
+                                "ignore": ["**/index.html"],
+                            },
+                        },
+                    ],
+                } ),
             ],
-        } ),
-    ],
-};
-
-export default config;
+        };
+    },
+    [new URL( "../schemas/env.main.schema.yaml", import.meta.url )]
+);
