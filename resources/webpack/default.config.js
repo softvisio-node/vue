192b0fa1d7373d7c571883b60d952f638540115d
zdm <zdm@softvisio.net>
2022-09-02 22:13:23 +0300

    fix: firebase worker

--- resources/webpack/default.config.js
+++ resources/webpack/default.config.js
@@ -1,7 +1,6 @@
 import main from "./main.config.js";
+import firebaseWorker from "./firebase.worker.config.js";
 
-// import firebaseWorker from "./firebase.worker.config.js";
-
-// main.add( firebaseWorker );
+main.add( firebaseWorker );
 
 export default main;
