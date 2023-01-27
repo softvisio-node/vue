562dacf8cf55efb356d8441f2e4b1c70b6d8789c
zdm <zdm@softvisio.net>
2023-01-27 07:18:32 +0200

    fix: webpack config options

--- resources/webpack/firebase-messaging.worker/index.js
+++ resources/webpack/firebase-messaging.worker/index.js
@@ -5,6 +5,14 @@ const worker = resolve( "#src/firebase/messaging/web.worker", import.meta.url );
 
 export class FirebaseMessagingWorker extends Super {
 
+    // properties
+    get resolveAlias () {
+        return {
+            ...super.resolveAlias,
+            "#vue": "@softvisio/vue",
+        };
+    }
+
     // protected
     _generate ( options ) {
         const config = super._generate( options );
