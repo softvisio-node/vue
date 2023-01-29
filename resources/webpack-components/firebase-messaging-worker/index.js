eb45d0c06f0533fd44598824cb2c9c8ccb0adbbf
zdm <zdm@softvisio.net>
2023-01-29 21:09:17 +0200

    fix: webpack components

--- resources/webpack-components/firebase-messaging-worker/index.js
+++ resources/webpack-components/firebase-messaging-worker/index.js
@@ -1,24 +1,17 @@
 import WebpackComponent from "@softvisio/webpack/components/firebase-messaging-worker";
 import { resolve } from "#core/utils";
 
-const worker = resolve( "#src/firebase/messaging/web.worker", import.meta.url );
-
 export default class extends WebpackComponent {
 
     // properties
+    get entryImport () {
+        return resolve( "#src/firebase/messaging/web.worker", import.meta.url );
+    }
+
     get resolveAlias () {
         return {
             ...super.resolveAlias,
             "#vue": "@softvisio/vue",
         };
-    }
-
-    // protected
-    _buildWebpackConfig ( options ) {
-        const config = super._buildWebpackConfig( options );
-
-        config.entry.firebase.import = worker;
-
-        return config;
     }
 }
