0f0286d9cc08a811c0ef045792cc727e0d7533ab
zdm <zdm@softvisio.net>
2023-01-30 20:16:09 +0200

    fix: webpack components

--- resources/webpack-components/firebase-messaging-worker/index.js
+++ resources/webpack-components/firebase-messaging-worker/index.js
@@ -1,11 +1,36 @@
-import WebpackComponent from "@softvisio/webpack/webpack-components/firebase-messaging-worker";
+import WebpackComponent from "@softvisio/webpack/webpack-components/web-worker";
 import { resolve } from "#core/utils";
+import fs from "node:fs";
+import path from "node:path";
 
 export default class extends WebpackComponent {
 
     // properties
+    get schemas () {
+        return [
+
+            //
+            ...super.schemas,
+            new URL( "env.schema.yaml", import.meta.url ),
+        ];
+    }
+
+    get isEnabled () {
+        if ( !super.isEnabled ) return false;
+
+        if ( this.isCordova ) return false;
+
+        if ( !this.appConfig.firebase?.web ) return false;
+
+        return true;
+    }
+
     get entryImport () {
         return resolve( "#src/firebase/messaging/web.worker", import.meta.url );
+    }
+
+    get entryFilename () {
+        return "firebase-messaging.worker.js";
     }
 
     get resolveAlias () {
@@ -13,5 +38,18 @@ export default class extends WebpackComponent {
             ...super.resolveAlias,
             "#vue": "@softvisio/vue",
         };
+    }
+
+    // protected
+    _init () {
+        super._init();
+
+        this.preprocessorParams.firebaseMessagingWorkerEnabled = this.isEnabled;
+
+        if ( this.isEnabled ) {
+            this.preprocessorParams.firebaseMessagingWorkerMixin = fs.existsSync( path.join( this.context, "src/firebase-messaging.worker.js" ) );
+
+            this.sharedResolveAlias["#firebaseMessagingWorker$"] = path.join( this.tmpPath, "firebase-messaging.worker.js" );
+        }
     }
 }
