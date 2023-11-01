a854ab78a8e0ed9425305ea4a11a7150c6105ef1
zdm <zdm@softvisio.net>
2023-11-01 10:02:58 +0200

    fix: permissions

--- src/app/user/permissions.js
+++ src/app/user/permissions.js
@@ -15,10 +15,13 @@ export default class {
 
         if ( this.#user.isRoot ) return true;
 
-        if ( !Array.isArray( permissions ) ) permissions = [permissions];
-
-        for ( const permission of permissions ) {
-            if ( this.#permissions.has( permission ) ) return true;
+        if ( Array.isArray( permissions ) ) {
+            for ( const permission of permissions ) {
+                if ( this.#permissions.has( permission ) ) return true;
+            }
+        }
+        else if ( this.#permissions.has( permissions ) ) {
+            return true;
         }
 
         if ( this.#oarentPermissions ) {
