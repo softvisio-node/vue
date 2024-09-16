615fe018bfdb37b9e21a613354f58a2a48805ea5
zdm <zdm@softvisio.net>
2024-09-16 11:30:31 +0300

    fix: app

--- src/app/user.js
+++ src/app/user.js
@@ -1,6 +1,6 @@
-import constants from "@softvisio/app/constants";
+import constants from "#core/app/constants";
 import { reactive } from "vue";
-import Permissions from "@softvisio/app/user/permissions";
+import Permissions from "#core/app/user/permissions";
 
 export default class User {
     #app;
